bc003c141b6e1d137d30addab07025625208aaf5
prinzsunny <prinzsunny07@gmail.com>
2018-05-20 23:16:06 -0700

    webpack

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+ app.get('/', function response(req, res) {
+ 	console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+
