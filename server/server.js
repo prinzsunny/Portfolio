f388ce37e5cb8addd255352bc892adfb27700e91
Prince Mukka <mukkapr1@l02c02td7s9gtfl.esurance.com>
2018-05-22 16:37:09 -0700

    Added HMR middleware

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import path from "path";
 
 const app = express();
@@ -10,6 +10,43 @@ app.use(express.static(path.resolve(__dirname, 'public')));
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
   });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => console.log("Example app listening on port 3000!"));*/
+
+const express = require('express');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.client.config.js');
+const path = require('path');
+const app = express();
+const compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'client.bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
+
+app.use(webpackHotMiddleware(compiler, {
+  log: console.log,
+  path: '/__webpack_hmr',
+  heartbeat: 10 * 1000,
+}));
+
+app.get('/', function response(req, res) {
+ 	console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, '../dist/public/index.html'));
+});
+
+const server = app.listen(3000, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
 
--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 const config = {
   entry: {
-    client: './client/client.js'
+    client: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+            './client/client.js']
   },
   target: 'web',
   mode:"development",
@@ -30,7 +32,9 @@ const config = {
     new HtmlWebpackPlugin({  // Also generate a test.html
       filename: 'index.html',
       template: path.resolve(__dirname, "assets/index.html")
-    }) ],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+ ],
   devtool: 'inline-source-map'
 }
 
